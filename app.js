23658a659d0752f21c164cdf4a374de7b3290f0d
prasanna <prasannaramesh98@gmail.com>
2022-02-18 11:33:40 +0530

    initial commits

---
+++ app.js
@@ -0,0 +1,8 @@
+const btnTranslate = document.querySelector('#btn-translate');
+const txtArea = document.querySelector('.txt-area');
+
+
+btnTranslate.addEventListener('click', ()=>{
+    console.log('clicked');
+    console.log(txtArea.value);
+});
